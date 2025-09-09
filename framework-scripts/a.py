f16a914a2cbb06818ec28046871e7a085c62c27d
ethanlu1788 <126304420+ethanlu1788@users.noreply.github.com>
2025-09-09 21:01:19 +1200

    framework models

---
+++ framework-scripts/a.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load your trained YOLOv11n model
+# This should be your original .pt file
+model = YOLO('../models/best.pt')
+
+# Export the model directly to TFLite format
+# This will create 'yolov11n_float32.tflite'
+model.export(format='openvino'
+                    '')
+
+print("Successfully exported to yolov11n_float32.tflite")
